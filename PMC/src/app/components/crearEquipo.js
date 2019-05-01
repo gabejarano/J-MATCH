5b772edfec132179e9bc06aa5a5ebf97a8b1cc6c
Gustavo Bejarano <ga.bejarano10@uniandes.edu.co>
2019-05-01 09:47:36 -0500

    mas cambios

--- PMC/src/app/components/crearEquipo.js
+++ PMC/src/app/components/crearEquipo.js
@@ -2,12 +2,32 @@ import React,{Component} from 'react';
 
 class CrearEquipo extends Component{
 
-
+    constructor(){
+        super();
+        this.state={
+            showCodigo:false,
+            showCrear:false
+        }
+        this.mostrarCodigo= this.mostrarCodigo.bind(this);
+        this.mostrarCrear= this.mostrarCrear.bind(this);
+      }
+      mostrarCodigo(e){
+        this.setState({
+            showCodigo:true,
+            showCrear: false
+        })
+      }
+      mostrarCrear(e){
+          this.setState({
+              showCodigo:false,
+              showCrear:true
+          })
+      }
     render(){
         return(
-            <div>
-                <br></br>
-                <br></br>
+            <div>  
+                <br></br> 
+                <br></br> 
                 <br></br>
                 <br></br>
                 <br></br>
@@ -25,26 +45,49 @@ class CrearEquipo extends Component{
                 <div className="service-area">
                      <div className="container">
                         <div className="row">
-                            <div className="col-md-4 col-sm-4 col-xs-12 col-6">
+                            <div  className="col-md-4 col-sm-4 col-xs-12 col-6">
                              <div className="service-single">
-                                <img src="assets/img/service/service-img1.png" alt="service image"/>
-                                <h2>Registra tu equipo</h2>
+                                <img src="src/app/assets/img/service/service-img1.png" alt="service image"/>
+                                <a onClick={this.mostrarCrear}><h2>Registra tu equipo</h2></a>
                                 <p>Toma la iniciativa de medir tu equipo</p>
                              </div>
                             </div>
-                            <div className="col-md-4 col-sm-4 col-xs-12 col-6">
-
-                            </div>
-                <div className="col-md-4 col-sm-4 col-xs-12 col-6">
+                            {this.state.showCodigo && <div className="col-md-4 col-sm-4 col-xs-12 col-6"> 
+                                                        <form>
+                                                            <div className="form-group">
+                                                                <input type="text" className="form-input" name="codigo"  placeholder="Codigo de quipo"/>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                <input type="submit" name="submit" className="form-submit"/>
+                                                            </div>
+                                                        </form>
+                                                    </div>}
+                            {this.state.showCrear && <div className="col-md-4 col-sm-4 col-xs-12 col-6"> 
+                                                        <form>
+                                                            <div className="form-group">
+                                                                <input type="text" className="form-input"  name="nombre equipo"  placeholder="Nombre equipo"/>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                <input type="text" className="form-input"  name="numero de integrantes"  placeholder="Numero de integrantes"/>
+                                                            </div>
+                                                            <div className="form-group">
+                                                                <input type="submit" name="submit" className="form-submit"/>
+                                                        </div>
+                                                        </form>
+                                                     </div>}
+                    
+                <div  className="col-md-4 col-sm-4 col-xs-12 col-6" > 
                     <div className="service-single">
-                        <img src="assets/img/service/service-img3.png" alt="service image"/>
-                        <h2>Ya tienes equipo</h2>
-                        <p>Ingresa codigo de tu equipo</p>
+                        <img src="src\app\assets\img\service\service-img3.png" alt="service image"/>
+                        <a onClick={this.mostrarCodigo}><h2>Ya tienes equipo</h2></a>
+                       <p>Ingresa codigo de tu equipo</p>
                     </div>
                 </div>
+                
             </div>
         </div>
     </div>
+                
     <br></br>
                 <br></br>
                 <br></br>
@@ -60,8 +103,8 @@ class CrearEquipo extends Component{
                 <br></br>
                 <br></br>
                 <br></br>
-                <br></br>
-            </div>
+                <br></br>   
+           </div>
         )
     }
 }
