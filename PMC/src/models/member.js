5cf04c6d836813bc08fa55d04aa306e6776dfb77
Gustavo Bejarano <ga.bejarano10@uniandes.edu.co>
2019-05-04 14:15:48 -0500

    conexion formularios

--- PMC/src/models/member.js
+++ PMC/src/models/member.js
@@ -3,6 +3,9 @@ const { Schema } = mongoose;
 
 const member = new Schema ({
         name: {type: String, required:true},
+        email: {type: String, required: true},
+        password: {type: String, required: true},
+        repassword: {type: String, required: true},
         age: {type: Number, required: false, min:18},
         sex: {type: String,required:false},
         document: {type: Number,required:false},
